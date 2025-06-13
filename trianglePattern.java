c34f1d0b8ec1c4c51718fa1145e038a21650339b
Madhuu4 <madhumitha.ur@gmail.com>
2025-06-13 10:27:38 +0530

    Initial commit

---
+++ trianglePattern.java
@@ -0,0 +1,14 @@
+public class trianglePattern {
+    public static void main(String[] args){
+        
+        // right angled triangle
+        for(int i=0;i<5;i++){
+            for(int j=0; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
